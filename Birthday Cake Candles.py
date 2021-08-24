c8f7605f63da7a3b578f01abc190cb288c54ee53
theCreator19 <77932869+theCreator19@users.noreply.github.com>
2021-08-24 19:52:10 +0530

    Create Birthday Cake Candles.py

---
+++ Birthday Cake Candles.py
@@ -0,0 +1,39 @@
+#  Birthday Cake Candles
+
+#  HackerRank Problem Solved using Python.
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    a=sorted(candles)
+    
+    b=a.count(a[len(candles)-1])
+    return b
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
